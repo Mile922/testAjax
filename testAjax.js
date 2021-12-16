9ad0ad13abbbe4aa7ac54bb0832001b8ad4a5414
wt627 <2214166877@qq.com>
2021-12-15 17:32:53 -0800

    second

--- testAjax.js
+++ testAjax.js
@@ -1,4 +1,6 @@
 const express=require("express")
 const app=express()
+var i=0;
 app.use('/',express.static('public'))
+app.get("/BtCount",(req,res)=>{i++;res.send(i.toString())})
 app.listen(10627)
